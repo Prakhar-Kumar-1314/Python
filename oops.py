1570b833d68507c675d6d8c8df3849d3cee8d40d
prakharkumar1314@gmail.com <prakharkumar1314@gmail.com>
2023-11-15 22:35:16 +0530

    Commit to dev

---
+++ oops.py
@@ -0,0 +1,23 @@
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#
+# p1 = Person("Prakhar", 46)
+# print(p1.name, p1.age)
+
+
+class Fruit:
+    def __init__(self, name, colours, calories):
+        self.name = name
+        self.colours = colours
+        self.calories = calories
+
+
+fruit1 = Fruit("Apple", "Red", 69)
+fruit2 = Fruit("Orange", "Orange", 112)
+fruit3 = Fruit("Banana", "Yellow", 56)
+
+print(fruit1.name, fruit1.colours, fruit1.calories)
+
