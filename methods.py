fdea4597327e7873ccf0171f91ad147388b3213d
prakharkumar1314@gmail.com <prakharkumar1314@gmail.com>
2023-11-17 21:55:37 +0530

    Commit to dev

--- methods.py
+++ methods.py
@@ -1,18 +1,28 @@
-class Person:
-    def __init__(self, name):
-        self.name = name
+class Rectangle:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
 
+    def get_position(self):
+        return (self.x, self.y)
 
-    def say_hello(self):
-        print(f"Hello, {self.name}")
+    def change_position(self, x_new, y_new):
+        self.x = x_new
+        self.y = y_new
+        return (x_new, y_new)
 
-    def set_age(self, age):
-        self.age = age
+    def get_area(self):
+        cal_area = self.width * self.height
+        return cal_area
 
-    def get_age(self):
-        return self.age
+rect = Rectangle(1, 2, 10, 12)
+pos = rect.get_position()
 
-p1 = Person("Prakhar")
-print(p1.name)
-p1.set_age(18)
-print(p1.get_age())
+pos = rect.change_position(2, 4)
+print(pos)
+
+area = rect.get_area()
+
+print(area)
